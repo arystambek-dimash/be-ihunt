514911b36372bd2167e42e9d7e29e993758f2459
arystambek-dimash <arystambekdimash005@gmail.com>
2024-08-05 09:25:10 +0500

    new routers

--- src/routers/vacany-routers.ts
+++ src/routers/vacany-routers.ts
@@ -5,6 +5,7 @@ import {
     getResponses,
     deletePosition,
     updatePositionStatus,
+    searchVacanciesByJobName,
 } from "../controllers/vacancy-controller";
 import authMiddleware from "../middlewares/auth-middleware";
 
@@ -15,5 +16,8 @@ router.get("/positions", authMiddleware, getPositions);
 router.get("/responses", authMiddleware, getResponses);
 router.delete("/positions/:positionId", authMiddleware, deletePosition);
 router.patch("/positions/:positionId/status", authMiddleware, updatePositionStatus);
+
+router.post('/search-vacancies', searchVacanciesByJobName);
+
 
 export default router;
